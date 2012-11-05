3b23cdcc3c5647b2e5778e21ba8cefff2dce2967
Burakhan <burakhanozyol@gmail.com>
2012-11-06 01:50:37 +0200

    Projenin İlk Oluşumu

---
+++ src/com/burakhan/ekders/EkDers.java
@@ -0,0 +1,24 @@
+package com.burakhan.ekders;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class EkDers extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ek_ders);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_ek_ders, menu);
+        return true;
+    }
+
+    
+}
