e733833f4e25f415f32ff0210bffa7ab56d21137
Brkhn <burakhanozyol@gmail.com>
2013-01-14 21:16:33 +0200

    En son

--- gen/com/burakhan/ekders/R.java
+++ gen/com/burakhan/ekders/R.java
@@ -35,7 +35,7 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
-        public static final int TextView01=0x7f090005;
+        public static final int TextView01=0x7f090004;
         public static final int button1=0x7f090003;
         public static final int gece=0x7f09000b;
         public static final int geceText=0x7f090010;
@@ -50,8 +50,8 @@ public final class R {
         public static final int ssk=0x7f090013;
         public static final int textView1=0x7f090001;
         public static final int textView2=0x7f09000f;
-        public static final int textView3=0x7f090006;
-        public static final int textView4=0x7f090004;
+        public static final int textView3=0x7f090005;
+        public static final int textView4=0x7f090006;
         public static final int textView5=0x7f09000a;
         public static final int tur=0x7f090002;
         public static final int vergiText=0x7f090011;
